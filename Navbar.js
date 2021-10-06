7a1df4efe7611222722d64871091e22db497970a
Piyawann <91889284+Piyawann@users.noreply.github.com>
2021-10-06 13:56:51 +0700

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Navbar() {
+  return (
+    <header className="Navbar" >
+     Facebook
+    </header>
+  );
+}
+
+export default Navbar;
